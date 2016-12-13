477759d092e1839b2e752919d8262e1dcde22edd
Bogdan Rancichi <bogdan.rancichi@emag.ro>
2016-12-13 20:22:30 +0200

    spaced out the doc comments

--- src/CacheBundle/Service/AbstractCache.php
+++ src/CacheBundle/Service/AbstractCache.php
@@ -1,8 +1,8 @@
 <?php
+
 namespace CacheBundle\Service;
 
 use CacheBundle\Exception\CacheException;
-use Predis\Client;
 
 abstract class AbstractCache
 {
@@ -10,74 +10,95 @@ abstract class AbstractCache
 
     /**
      * Tries to add new key
-     * @param $key
-     * @param $value
-     * @param int $ttl
-     * @return bool
-     * @throws CacheException
+     *
+     * @param   string  $key
+     * @param   mixed   $value
+     * @param   int     $ttl
+     *
+     * @return  bool
+     *
+     * @throws  CacheException
      */
     abstract public function add($key, $value, $ttl = 600);
+
     /**
-     * @param $key
-     * @param $value
-     * @param int $ttl
-     * @return bool
-     * @throws CacheException
+     * @param   string  $key
+     * @param   mixed   $value
+     * @param   int     $ttl
+     *
+     * @return  bool
+     *
+     * @throws  CacheException
      */
     abstract public function set($key, $value, $ttl = 600);
 
     /**
      * Checks if a cache key exists
-     * @param $key
-     * @return bool
+     *
+     * @param   string  $key
+     *
+     * @return  bool
      */
     abstract public function has($key);
 
     /**
      * Retrieves info from a cache key
-     * @param $key
-     * @return mixed
+     *
+     * @param   string  $key
+     *
+     * @return  mixed
      */
     abstract public function get($key);
 
     /**
      * Retrieves the ttl of the specified cache key
-     * @param $key
-     * @return int
+     *
+     * @param   string  $key
+     *
+     * @return  int
      */
     abstract public function ttl($key);
 
     /**
      * Deletes the cache key. If key does not exists, it will throw CacheException
-     * @param $key
-     * @return mixed
-     * @throws CacheException
+     *
+     * @param   string  $key
+     *
+     * @return  mixed
+     *
+     * @throws  CacheException
      */
     abstract public function delete($key);
 
     /**
      * Increases the ttl of the cache
-     * @param $key
-     * @param int $ttl
+     *
+     * @param   string  $key
+     * @param   int     $ttl
+     *
+     * @return  void
      */
     abstract public function refreshTtl($key, $ttl = 3600);
 
     /**
-     * @param $key
-     * @param int $ttl
-     * @return bool
-     * @throws CacheException
+     * @param   string  $key
+     * @param   int     $ttl
+     *
+     * @return  bool
+     *
+     * @throws  CacheException
      */
     final public function lock($key, $ttl = 3600)
     {
         return $this->add(self::LOCK_PREFIX . $key, 1, $ttl);
     }
 
-
     /**
-     * @param $key
-     * @return bool
-     * @throws CacheException
+     * @param   string  $key
+     *
+     * @return  bool
+     *
+     * @throws  CacheException
      */
     final public function unlock($key)
     {
@@ -85,8 +106,9 @@ abstract class AbstractCache
     }
 
     /**
-     * @param $key
-     * @return bool
+     * @param   string  $key
+     *
+     * @return  bool
      */
     final public function hasLock($key)
     {
@@ -94,8 +116,10 @@ abstract class AbstractCache
     }
 
     /**
-     * @param $key
-     * @param int $ttl
+     * @param   string  $key
+     * @param   int     $ttl
+     *
+     * @return  void
      */
     final   public function heartBeatLock($key, $ttl = 3600)
     {
