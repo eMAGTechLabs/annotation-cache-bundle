3d282b525475890b052138c42be97cd771eb9c97
Bogdan Rancichi <bogdan.rancichi@emag.ro>
2016-12-13 19:11:05 +0200

    moved to a more standard symfony path

---
+++ src/CacheBundle/CacheWarmer/ProxyWarmer.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace CacheBundle\CacheWarmer;
+
+use CacheBundle\ProxyManager\Factory\ProxyCachingObjectFactory;
+use Symfony\Component\HttpKernel\CacheWarmer\CacheWarmerInterface;
+
+class ProxyWarmer implements CacheWarmerInterface
+{
+    /**
+     * @var  ProxyCachingObjectFactory
+     */
+    protected $factory;
+
+    /**
+     * @var array
+     */
+    protected $classes = [];
+
+    public function setFactory(ProxyCachingObjectFactory $factory)
+    {
+        $this->factory = $factory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function isOptional()
+    {
+        return false;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function warmUp($cacheDir)
+    {
+        foreach ($this->classes as $class) {
+            $this->factory->createProxy($class);
+        }
+    }
+
+    /**
+     * @param   string  $className
+     *
+     * @return  void
+     */
+    public function addClassToGenerate($className)
+    {
+        $this->classes[$className] = $className;
+    }
+}
